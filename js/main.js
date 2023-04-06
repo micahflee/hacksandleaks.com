d1d3f6a35fc088e657679ca9291516824c1cf52e
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-06 16:38:02 -0700

    mobile responsiveness

--- js/main.js
+++ js/main.js
@@ -42,3 +42,15 @@ function countWords() {
   const readingTimeElement = document.getElementById('reading-time');
   readingTimeElement.innerText = readingTimeText;
 }
+
+$(document).ready(function() {
+  $('.btnIcon').on('click', function() {
+    $('nav ul').toggleClass('show');
+  });
+
+  $('nav li').on('click', function() {
+    $('header nav ul').removeClass('show');
+  });
+});
+
+
