4cf335bbf30a9db2456294cda0dafe42dca9a54d
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-18 20:03:01 -0700

    cipher intro

--- js/main.js
+++ js/main.js
@@ -43,6 +43,38 @@ function countWords() {
   readingTimeElement.innerText = readingTimeText;
 }
 
+function generateRandomString(length) {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return result;
+}
+
+function cipherAnimation(element, duration) {
+  const originalText = element.textContent;
+  const cipherText = generateRandomString(originalText.length);
+  element.textContent = cipherText;
+
+  let decipherProgress = 0;
+  const decipherInterval = setInterval(function () {
+    const progressPercentage = decipherProgress / originalText.length;
+    const newText = originalText.slice(0, decipherProgress) + cipherText.slice(decipherProgress);
+    element.textContent = newText;
+    decipherProgress++;
+
+    if (decipherProgress > originalText.length) {
+      clearInterval(decipherInterval);
+    }
+  }, duration / originalText.length);
+}
+
+const title = document.querySelector(".cipher-title");
+cipherAnimation(title, 2000); // You can adjust the duration as desired.
+title.classList.add("active");
+
+
 $(document).ready(function() {
   $('.btnIcon').on('click', function() {
     $('nav ul').toggleClass('show');
